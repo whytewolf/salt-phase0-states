b638daba9f5991b24760ba57f6ded009e90a04ef
whytewolf <github@whytewolf.us>
2016-12-29 15:17:27 -0800

    start of stop

--- _modules/rndc.py
+++ _modules/rndc.py
@@ -143,4 +143,8 @@ def secroots(view=None,server=None,key=None):
         cmd = _add_option(cmd,'in {0}'.format(view))
     return __salt__['cmd.run'](cmd)
 
-
+def stop(server=None,key=None):
+    cmd = _auth_options(server,key)
+    cmd = _add_option(cmd,'stop -p')
+    pid = __salt__['cmd.run'](cmd)
+    return pid
