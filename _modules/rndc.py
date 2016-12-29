706727459a91e679c2871915db2c8c959796f4f9
whytewolf <github@whytewolf.us>
2016-12-29 14:45:38 -0800

    add freeze and thaw

--- _modules/rndc.py
+++ _modules/rndc.py
@@ -68,3 +68,22 @@ def loadkeys(zone,view=None,server=None,key=None):
     if view is not None:
         cmd - _add_option(cmd,'in {0}'.format(view))
     return __salt__['cmd.run'](cmd)
+
+def freeze(zone=None,view=None,server=None,key=None):
+    cmd = _auth_options(server,key)
+    cmd = _add_option(cmd,'freeze')
+    if zone is not None:
+        cmd = _add_option(cmd,zone)
+    if view is not None:
+        cmd - _add_option(cmd,'in {0}'.format(view))
+    return __salt__['cmd.run'](cmd)
+
+def thaw(zone=None,view=None,server=None,key=None):
+    cmd = _auth_options(server,key)
+    cmd = _add_option(cmd,'thaw')
+    if zone is not None:
+        cmd = _add_option(cmd,zone)
+    if view is not None:
+        cmd - _add_option(cmd,'in {0}'.format(view))
+    return __salt__['cmd.run'](cmd)
+
