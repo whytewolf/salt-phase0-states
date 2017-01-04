9796b3284dd131d680e488c4be3cb9e35b2541ea
whytewolf <github@whytewolf.us>
2017-01-04 14:30:42 -0800

    trying to add some default quoteing

--- _modules/rndc.py
+++ _modules/rndc.py
@@ -246,7 +246,7 @@ def addzone(zone,config,view=None,server=None,key=None):
     cmd = _add_option(cmd,zone)
     if view is not None:
         cmd = _add_option(cmd,'in {0}'.format(view))
-    cmd = _add_option(cmd,config)
+    cmd = _add_option(cmd,'\''+config+'\'')
     log.debug('cmd.run {0}'.format(cmd))
     return __salt__['cmd.run'](cmd)
 
